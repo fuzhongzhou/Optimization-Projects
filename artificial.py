6bdcd9a2223b55cd8909eff70f599d3d11ceeaeb
Fuzhong Zhou <40592542+fuzhongzhou@users.noreply.github.com>
2020-10-11 18:53:24 -0400

    modify artifical number

--- artificial.py
+++ artificial.py
@@ -4,44 +4,57 @@ import matplotlib.pyplot as plt
 
 N = 252*2   # period
 T = 2       # Year
-k = 20      # num of factors
+k = 10      # num of factors
 p = 30      # num of securities
 S0 = np.random.randn(p) + 10
 
-# without factor, using geometric Brownian Motion here
+train_ratio = 0.8
+train_index = int(N*train_ratio)
+
+
+''' without factor, using geometric Brownian Motion here '''
 rf = 0.03
 np.random.seed(0)
-BM_cov = np.random.randn(p)
-BM_cov = BM_cov.reshape((-1, 1)) @ BM_cov.reshape((1, -1)) / 10
+
+BM_corr = pd.DataFrame(np.random.randn(20, p)).corr().to_numpy()
+sig = np.diag(np.random.uniform(0.15, 0.3, p))
+BM_cov = sig @ BM_corr @ sig
 delta = T / N
 S = np.zeros((N+1, p))
 S[0] = S0
 z = np.random.multivariate_normal(np.zeros(p), BM_cov, N)
 for i in range(N):
     S[i+1] = S[i] * np.exp((rf-(BM_cov**2).sum(axis=1)/2)*delta + np.sqrt(delta)*z[i])
-
 r = (S[1:] - S[:-1]) / S[:-1]
 
-train_ratio = 0.8
-train_index = int(N*train_ratio)
+
 r_train = r[:train_index]
 r_test = r[train_index:]
 res_nonlinear = eval((r_train,r_test),delta)
+print(res_nonlinear)
 
 
-# with linear factor structure
-f_cov = np.random.randn(k)
-f_cov = f_cov.reshape((-1, 1)) @ f_cov.reshape((1, -1))
-f_mean = np.random.randn(k)
-f = np.random.multivariate_normal(f_mean, f_cov, N)
+''' with linear factor structure '''
+f_corr = pd.DataFrame(np.random.randn(20, k)).corr().to_numpy()
+f_sig = np.diag(np.random.uniform(0.01, 0.02, k))
+f_cov = f_sig @ f_corr @ f_sig
+f_start = np.random.uniform(-0.4, 0.4, k)
 
-beta = np.random.randn(k, p) / 100
-u = np.random.randn(N, p) / 1000
+f = np.zeros((N+1, k))
+f[0] = f_start
+z = np.random.multivariate_normal(np.zeros(k), f_cov, N)
+for i in range(N):
+    f[i+1] = f[i] + z[i]
+
+f = f[1:]
+# pl.plot(f)
+beta = np.random.uniform(-0.02, 0.02, (k, p))
+u = np.random.randn(N, p) / 100
 r = f @ beta + u
 
 r_train = r[:train_index]
 r_test = r[train_index:]
 res_linear = eval((r_train,r_test),delta,f[:train_index])
-
+print(res_linear)
 
 print(0)
